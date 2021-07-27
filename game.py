2c2ebcb0c4dfc6fd8d068b78fffddc3b804b0ecc
Tushar-Edge <60666262+Tushar-Edge@users.noreply.github.com>
2021-07-27 19:32:30 +0530

    inital

---
+++ game.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+
+x=1
+remaining_stars=21
+print("stars",21*' * ')
+while(x<=4):
+    u=int(input("its UR CHANCE "))
+    O=5-u
+    remaining_stars=remaining_stars-5
+    print("computer=",O)
+    print("\t stars left=",remaining_stars*' * ')
+    x=x+1
+print("now u have to remove the last one ..u lose..try again")    
+    
+    
